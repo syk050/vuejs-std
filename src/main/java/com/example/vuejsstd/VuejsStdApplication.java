b76decf947591a55b4b80868279f8e6309e40a41
syk050 <syk050@naver.com>
2022-08-05 18:03:45 +0900

    Initial commit

---
+++ src/main/java/com/example/vuejsstd/VuejsStdApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuejsstd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuejsStdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VuejsStdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vuejsstd/VuejsStdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuejsstd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuejsStdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
