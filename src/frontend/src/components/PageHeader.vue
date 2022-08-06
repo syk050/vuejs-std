9dfc5b49d85d1befecb2ce0691c1286de206254d
syk050 <syk050@naver.com>
2022-08-07 00:09:02 +0900

    프론트엔트 초기 구성

--- src/frontend/src/components/PageHeader.vue
+++ src/frontend/src/components/PageHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <header>
     <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
       <router-link to="/board/list">게시판</router-link>
     </div>
   </header>
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,4 +1,9 @@
+import './assets/common.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "./router";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// #app => index.html에 있는 id가 app인 곳에 표현
+app.use(router).mount('#app')
