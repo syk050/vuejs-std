b5ad94e6ac18562854c9ab204c3fca037f07228c
syk050 <syk050@naver.com>
2022-08-07 23:35:36 +0900

    main.js axios, url 특성

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -3,7 +3,12 @@ import './assets/common.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
+import axios from "axios";
 
 const app = createApp(App)
+
+app.config.globalProperties.$axios = axios;
+app.config.globalProperties.$serverUrl = "http://localhost:8081";
+
 // #app => index.html에 있는 id가 app인 곳에 표현
 app.use(router).mount('#app')
