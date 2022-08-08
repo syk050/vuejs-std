20158da1145fe6957b092e2be33eccb6e3c1d4c0
syk050 <syk050@naver.com>
2022-08-08 16:07:20 +0900

    DB 연동 테스트 코드

---
+++ src/test/java/com/example/vuejsstd/entity/BoardRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.vuejsstd.entity;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class BoardRepositoryTest {
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Test
+    public void LoadBoard() {
+        List<BoardEntity> boardList = boardRepository.findAll();
+
+        BoardEntity board = boardList.get(0);
+        assertThat(board.getTitle()).isEqualTo("게시글 제목1");
+    }
+
+
+}
---
+++ src/test/java/com/example/vuejsstd/web/BoardControllerTest.java
@@ -0,0 +1,58 @@
+package com.example.vuejsstd.web;
+
+import com.example.vuejsstd.entity.BoardEntity;
+import com.example.vuejsstd.entity.BoardRepository;
+import com.example.vuejsstd.web.dto.BoardDto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // 작성해야 TestRestTemplate 에러 안남
+class BoardControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    @Test
+    public void Board_Up() throws Exception {
+        String title = "Test Title";
+        String contents = "Test contents";
+        String author = "Test author";
+
+        BoardDto boardDto = BoardDto.builder()
+                .title(title)
+                .contents(contents)
+                .author(author)
+                .build();
+
+        String url = "http://localhost:" + port + "/board";
+
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, boardDto, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<BoardEntity> all = boardRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContents()).isEqualTo(contents);
+    }
+}
