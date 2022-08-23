ffefc56e1758e420e6a46f2fe61a0ac449c81ddc
syk050 <syk050@naver.com>
2022-08-24 01:01:29 +0900

    테스트 코드 삭제

--- src/main/java/com/example/vuejsstd/VuejsStdApplication.java
+++ src/main/java/com/example/vuejsstd/VuejsStdApplication.java
@@ -16,43 +16,13 @@ import java.util.List;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class VuejsStdApplication implements CommandLineRunner{
+public class VuejsStdApplication{
 
     @Autowired
     private CustomerRepository repository;
 
     public static void main(String[] args){
         SpringApplication.run(VuejsStdApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        repository.deleteAll();
-
-        // save a couple of customers
-        repository.save(new Customer("Alice", "Smith"));
-        repository.save(new Customer("Bob", "Smith"));
-
-        // fetch all customers
-        System.out.println("Customers found with findAll():");
-        System.out.println("-------------------------------");
-        for (Customer customer : repository.findAll()) {
-            System.out.println(customer);
-        }
-        System.out.println();
-
-        // fetch an individual customer
-        System.out.println("Customer found with findByFirstName('Alice'):");
-        System.out.println("--------------------------------");
-        System.out.println(repository.findByFirstName("Alice"));
-
-        System.out.println("Customers found with findByLastName('Smith'):");
-        System.out.println("--------------------------------");
-        for (Customer customer : repository.findByLastName("Smith")) {
-            System.out.println(customer);
-        }
-
     }
 
 }
